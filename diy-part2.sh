3e55d64e30fbe61266bbfdee9fa5a918ae10d299
zipated <zhuo392592689@live.com>
2020-08-02 18:53:57 +0800

    orange_pi_zero_plus_official_ver_test02

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+!/bin/bash
 #============================================================
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
@@ -10,3 +10,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.1.224/g' package/base-files/files/bin/config_generate
